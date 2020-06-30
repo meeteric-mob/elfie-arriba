757ade1b0751acd1913ce873b3c3cf30ef9c2ebe
Eric Maino <eric.maino@microsoft.com>
2020-06-30 13:55:43 -0700

    PLATFORM: Remove non-Core supported features

--- Arriba/Arriba.Server/Program.cs
+++ Arriba/Arriba.Server/Program.cs
@@ -1,17 +1,18 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Arriba.Monitoring;
+using Arriba.Server.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using System;
 using System.Diagnostics;
-using System.Net;
-
-using Arriba.Monitoring;
-using Arriba.Server.Owin;
-
-using Microsoft.Owin.Builder;
-using Microsoft.Owin.Hosting;
-
-using Owin;
+using AspNetHost = Microsoft.Extensions.Hosting.Host;
 
 namespace Arriba.Server
 {
@@ -35,26 +36,55 @@ namespace Arriba.Server
             // Always log to CSV
             EventPublisher.AddConsumer(new CsvEventConsumer());
 
-            using (var app = WebApp.Start<SelfHostArribaOwinStartup>(String.Format("http://*:{0}/", portNumber)))
-            {
-                Console.WriteLine("Running... Press any key to exit.");
-                Console.ReadKey();
-            }
+            CreateHostBuilder(args).Build().Run();
 
             Console.WriteLine("Exiting.");
             Environment.Exit(0);
         }
-    }
 
-    public class SelfHostArribaOwinStartup : ArribaOwinStartup
-    {
-        public override void Configuration(IAppBuilder app)
+        private static IHostBuilder CreateHostBuilder(string[] args)
         {
-            base.Configuration(app);
+            return AspNetHost.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+        }
 
-            // Enable self host NTLM authentication 
-            var listener = (HttpListener)app.Properties[typeof(HttpListener).FullName];
-            listener.AuthenticationSchemes = AuthenticationSchemes.Negotiate;
+        private class Startup
+        {
+            // This method gets called by the runtime. Use this method to add services to the container.
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+            public void ConfigureServices(IServiceCollection services)
+            {
+            }
+
+            // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+            public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+            {
+                if (env.IsDevelopment())
+                {
+                    app.UseDeveloperExceptionPage();
+                }
+
+                var host = new Arriba.Server.Hosting.Host();
+                host.Add<JsonConverter>(new StringEnumConverter());
+                host.Compose();
+
+                var server = host.GetService<ComposedApplicationServer>();
+
+                app.Run(async context =>
+                {
+                    server.HandleAsync(context.R)
+                })
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapGet("/", async context =>
+                    {
+                        await context.Response.WriteAsync("Hello World!");
+                    });
+                });
+            }
         }
     }
 }
