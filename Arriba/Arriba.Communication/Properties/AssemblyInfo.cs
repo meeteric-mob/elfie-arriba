d59d6101cb8728e5948315321470fbd980cbcc2a
Eric Maino <ericmai@microsoft.com>
2020-06-30 13:40:08 -0700

    PLATFORM: Updating most projects to .NET Core

--- Arriba/Arriba.Communication/Properties/AssemblyInfo.cs
+++ Arriba/Arriba.Communication/Properties/AssemblyInfo.cs
@@ -8,11 +8,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Arriba.Communication")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Arriba.Communication")]
 [assembly: AssemblyCopyright("Copyright \u00A9  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -24,16 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("29bfa92d-2578-477f-bcc5-3437224d059e")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
