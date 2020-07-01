d580c89e6771387bee078388aac904425ed2a98e
Eric Maino <ericmai@microsoft.com>
2020-06-30 19:53:45 -0700

    REFACTOR: Introduce ArribaHttpContextRequest
    - Add support for both Owin and HttpContext in order to enable aspnet
      controller support moving forward
    - Note this implementation is not fully flushed out as things like
      an identity will need to be added once OAuth support exists

--- Arriba/Arriba.Communication/Server/Owin/ArribaOwinRequest.cs
+++ Arriba/Arriba.Communication/Server/Owin/ArribaOwinRequest.cs
@@ -13,64 +13,17 @@ using Arriba.Communication;
 
 namespace Arriba.Server.Owin
 {
-    public class ArribaOwinRequest : IRequest
+    public class ArribaOwinRequest : ArribaRequest
     {
         private IDictionary<string, object> _environment;
-        private IContentReaderWriterService _readerWriter;
-
-        private Lazy<RequestVerb> _verbLazy = null;
-        private Lazy<string> _pathLazy = null;
-        private Lazy<IValueBag> _queryStringLazy = null;
-        private Lazy<IValueBag> _headersLazy = null;
-        private Lazy<string[]> _acceptLazy = null;
-
-        private static readonly string[] s_emptyStringArray = new string[0];
 
         public ArribaOwinRequest(IDictionary<string, object> environment, IContentReaderWriterService readerWriter)
+            : base(readerWriter)
         {
             _environment = environment;
-            _readerWriter = readerWriter;
-
-            _verbLazy = new Lazy<RequestVerb>(this.GetVerb);
-            _pathLazy = new Lazy<string>(() => _environment.Get<string>("owin.RequestPath"));
-            _queryStringLazy = new Lazy<IValueBag>(() => new NameValueCollectionValueBag(HttpUtility.ParseQueryString(_environment.Get<string>("owin.RequestQueryString"), Encoding.UTF8)));
-            _headersLazy = new Lazy<IValueBag>(() => new DictionaryValueBag(_environment.Get<IDictionary<string, string[]>>("owin.RequestHeaders")));
-            _acceptLazy = new Lazy<string[]>(this.GetAcceptHeaders);
         }
 
-        public RequestVerb Method
-        {
-            get
-            {
-                return _verbLazy.Value;
-            }
-        }
-
-        public string Resource
-        {
-            get
-            {
-                return _pathLazy.Value;
-            }
-        }
-
-        public IValueBag ResourceParameters
-        {
-            get
-            {
-                return _queryStringLazy.Value;
-            }
-        }
-
-        public IValueBag Headers
-        {
-            get
-            {
-                return _headersLazy.Value;
-            }
-        }
-
-        public IPrincipal User
+        public override IPrincipal User
         {
             get
             {
@@ -78,23 +31,14 @@ namespace Arriba.Server.Owin
             }
         }
 
-        public string Origin
+        public override string Origin
         {
             get
             {
                 return _environment.Get<string>("server.RemoteIpAddress");
             }
         }
-
-        public bool HasBody
-        {
-            get
-            {
-                return this.Headers.Contains("Content-Length") && this.Headers["Content-Length"] != "0";
-            }
-        }
-
-        public Stream InputStream
+        public override Stream InputStream
         {
             get
             {
@@ -102,65 +46,27 @@ namespace Arriba.Server.Owin
             }
         }
 
-        public System.Threading.Tasks.Task<T> ReadBodyAsync<T>()
-        {
-            var reader = _readerWriter.GetReader<T>(this.Headers["Content-Type"]);
-            return reader.ReadAsync<T>(this.InputStream);
-        }
-
-        public IEnumerable<string> AcceptedResponseTypes
+        protected override string HttpRequestVerb
         {
             get
             {
-                return _acceptLazy.Value;
+                return _environment.Get<string>("owin.RequestMethod");
             }
         }
 
-        private RequestVerb GetVerb()
+        protected override string GetRequestPath()
         {
-            var verb = _environment.Get<string>("owin.RequestMethod");
-            switch (verb)
-            {
-                case "GET":
-                    return RequestVerb.Get;
-                case "POST":
-                    return RequestVerb.Post;
-                case "DELETE":
-                    return RequestVerb.Delete;
-                case "OPTIONS":
-                    return RequestVerb.Options;
-                case "PUT":
-                    return RequestVerb.Put;
-                case "PATCH":
-                    return RequestVerb.Patch;
-                default:
-                    throw new ArgumentException("Unknown HTTP Verb \"" + verb + "\"");
-            }
+            return _environment.Get<string>("owin.RequestPath");
         }
 
-        private string[] GetAcceptHeaders()
+        protected override IValueBag GetQueryString()
         {
-            string[] acceptRaw;
+            return new NameValueCollectionValueBag(HttpUtility.ParseQueryString(_environment.Get<string>("owin.RequestQueryString"), Encoding.UTF8));
+        }
 
-            if (this.Headers.TryGetValues("Accept", out acceptRaw) && acceptRaw != null && acceptRaw.Length > 0)
-            {
-                string[] items = acceptRaw.SelectMany(a => a.Split(',')).ToArray();
-
-                for (int i = 0; i < items.Length; i++)
-                {
-                    int paramsStartIndex = items[i].IndexOf(';');
-
-                    if (paramsStartIndex != -1)
-                    {
-                        // Strip accept params (e.g. q=xxx) 
-                        items[i] = items[i].Substring(0, paramsStartIndex).Trim();
-                    }
-                }
-
-                return items;
-            }
-
-            return s_emptyStringArray;
+        protected override IValueBag GetRequestHeaders()
+        {
+            return new DictionaryValueBag(_environment.Get<IDictionary<string, string[]>>("owin.RequestHeaders"));
         }
     }
 }
---
+++ Arriba/Arriba.Server/ArribaHttpContextRequest.cs
@@ -0,0 +1,82 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Text;
+using System.Web;
+
+using Arriba.Communication;
+using Microsoft.AspNetCore.Http;
+
+namespace Arriba.Server.Owin
+{
+    public class ArribaHttpContextRequest : ArribaRequest
+    {
+        private readonly HttpContext _context;
+
+        public ArribaHttpContextRequest(HttpContext context, IContentReaderWriterService readerWriter)
+            : base(readerWriter)
+        {
+            _context = context;
+        }
+
+        public override IPrincipal User
+        {
+            get
+            {
+                return new GenericPrincipal(new GenericIdentity("Anonymous"), Array.Empty<string>());
+            }
+        }
+
+        public override string Origin
+        {
+            get
+            {
+                //Remote ip address;
+                return string.Empty;
+            }
+        }
+        public override Stream InputStream
+        {
+            get
+            {
+                return _context.Request.Body;
+            }
+        }
+
+        protected override string HttpRequestVerb
+        {
+            get
+            {
+                return _context.Request.Method;
+            }
+        }
+
+        protected override string GetRequestPath()
+        {
+
+            return _context.Request.Path;
+        }
+
+        protected override IValueBag GetQueryString()
+        {
+            return new NameValueCollectionValueBag(HttpUtility.ParseQueryString(_context.Request.QueryString.ToUriComponent(), Encoding.UTF8));
+        }
+
+        protected override IValueBag GetRequestHeaders()
+        {
+            return new DictionaryValueBag(AsDictionary(_context.Request.Headers));
+        }
+
+        private IDictionary<string, string[]> AsDictionary(IHeaderDictionary headers)
+        {
+            return new Dictionary<string, string[]>(headers.Select(x => new KeyValuePair<string, string[]>(x.Key, x.Value.ToArray())));
+        }
+    }
+}
