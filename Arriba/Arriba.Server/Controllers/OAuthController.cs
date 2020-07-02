c3c2f0b9efe388fe4767fa3523b2de0f10c9f46e
CSE ART Mob Programming <mobbing@meeteric.net>
2020-07-02 15:48:31 -0700

    Adding simple oauth controller

---
+++ Arriba/Arriba.Server/Controllers/OAuthController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Arriba.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OAuthController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            const string redirect = "https://localhost:42784/signin-redirect";
+            const string tenant = "c3611820-5bdd-4423-a1fc-18834a47ae78";
+            const string appId = "051ef594-8e5a-4156-a8ce-93fae3220779";
+            return this.Redirect($"https://login.microsoftonline.com/{tenant}/oauth2/v2.0/authorize?client_id={appId}&response_type=id_token&redirect_uri={redirect}&scope=openid&response_mode=fragment&state=12345&nonce=678910");
+        }
+
+    }
+}
