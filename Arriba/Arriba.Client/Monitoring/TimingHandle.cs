042bc00c6ceb9dc8bdb06876e4134bd1ccacc1e0
mattklup <mattklup@microsoft.com>
2020-07-01 17:09:20 -0700

    Update TimingHandle to core

--- Arriba/Arriba.Client/Monitoring/TimingHandle.cs
+++ Arriba/Arriba.Client/Monitoring/TimingHandle.cs
@@ -2,78 +2,45 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 
 namespace Arriba.Monitoring
 {
     /// <summary>
-    /// Provides a mechanism to time a scope of execution in fractional milliseconds. 
+    /// Provides a mechanism to time a scope of execution in fractional milliseconds.
     /// </summary>
     internal class TimingHandle : IDisposable
     {
-        private static long s_qpcFrequency = 0;
-
-        private class NativeMethods
-        {
-            [DllImport("kernel32.dll")]
-            public static extern bool QueryPerformanceCounter(out long lpPerformanceCount);
-
-            [DllImport("kernel32.dll")]
-            public static extern bool QueryPerformanceFrequency(out long lpFrequency);
-        }
-
-        private long _start = 0L;
-        private long _stop = 0L;
-        private double _accumulated = 0D;
+        private Stopwatch _stopwatch;
+        private double _accumulated;
 
         internal TimingHandle()
         {
-            if (s_qpcFrequency == 0)
-            {
-                // NOTE: This is static for the lifetime of a machine 
-                NativeMethods.QueryPerformanceFrequency(out s_qpcFrequency);
-            }
-
-            NativeMethods.QueryPerformanceCounter(out _start);
+            // TODO: since QueryPerformanceCounter/QueryPerformanceFrequency are not available on dotnetcore
+            // we will use Stopwatch for now.  Determine if we can remove in favor of Stopwatch permantly of if
+            // timing is sensative enough we need a replacement
+            _stopwatch = Stopwatch.StartNew();
         }
 
         internal double ElapsedMiliseconds
         {
             get
             {
-                double running = 0;
-
-                if (_start > 0)
-                {
-                    long current;
-                    NativeMethods.QueryPerformanceCounter(out current);
-                    running = QpcRangeToMilliseconds(_start, current);
-                }
-
-                return running + _accumulated;
+                return _stopwatch.ElapsedMilliseconds;
             }
-        }
-
-        private static double QpcRangeToMilliseconds(long start, long stop)
-        {
-            return (double)(stop - start) * 1000.0 / (double)s_qpcFrequency;
         }
 
         internal void Start()
         {
-            if (_start == 0)
-            {
-                NativeMethods.QueryPerformanceCounter(out _start);
-                _stop = 0;
-            }
+            _stopwatch.Restart();
         }
 
         public void Dispose()
         {
-            NativeMethods.QueryPerformanceCounter(out _stop);
-            _accumulated = QpcRangeToMilliseconds(_start, _stop);
-            _start = 0;
-            _stop = 0;
+            // Old behavior was to restart so do that
+            _stopwatch.Stop();
+            _accumulated += this.ElapsedMiliseconds;
         }
     }
 }
