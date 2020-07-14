888abdc30a87d3ce4808ff7b9e565387d3c72f65
Vinicius Souza <visouza@microsoft.com>
2020-07-13 22:19:43 -0700

    Adding Interface IArribaManagementService

---
+++ src/Arriba/Arriba.Communication/Server/Application/IArribaManagementService.cs
@@ -0,0 +1,12 @@
+﻿using Arriba.Types;
+using System.Collections.Generic;
+
+namespace Arriba.Communication.Server.Application
+{
+    public interface IArribaManagementService
+    {
+        IEnumerable<string> GetTables();
+
+        Dictionary<string, TableInformation> GetAllBasic();
+    }
+}
